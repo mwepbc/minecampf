6195066d84370a91a53caf145f409250224eb332
mwepbc <just.natsuki@bk.ru>
2025-10-06 21:09:02 +0500

    replace all divs with cells on table. not srtuctured yet

--- index.php
+++ index.php
@@ -6,9 +6,10 @@ include("assets/functions/items/select.php");
 
 
 ?>
-  
+
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,57 +20,92 @@ include("assets/functions/items/select.php");
 <body>
     <?php include("assets/includes/header.php") ?>
     <main>
-        <div class="list">
-            <?php 
-            foreach ($crafts as $craft) {
-                $crafting_item = defeniteItem($dbh, $craft['crafting_item']);
+        <table class="list">
+            <thead>
+                <tr>
+                    <th colspan="3" class="search">
+                        <img src="assets/img/search.webp" alt="search">
+                        <input type="search">
+                    </th>
+                    <th colspan="2" class="filter">
+                        <img src="assets/img/Recipe Button.png" alt="filter">
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="cell"></td>
+                    <td class="cell"></td>
+                    <td class="cell"></td>
+                </tr>
+                <tr>
+                    <td class="cell"></td>
+                    <td class="cell"></td>
+                    <td class="cell"></td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th colspan="2"></th>
+                    <th>
+                        <h4>0/0</h4>
+                    </th>
+                    <th colspan="2">
+                        <img src="assets/img/arrow.png" alt="next">
+                    </th>
+                </tr>
+            </tfoot>
+        </table>
 
-                echo '
-                    <div class="cell">
-                        <input type="hidden" name="select" value="'.$craft['id'].'">
-                        <img src="assets/img/'. $crafting_item['image'].'" 
-                        alt="'. $crafting_item['name'].'">
-                    </div>';
-            }
-            ?>
-            
-            
-        </div>
-        
-        <div class="craftingTable">
-            <div class="cell">
-                
+        <table class="craftingTable">
+            <tr>
+                <th class="cell"></th>
+                <th class="cell"></th>
+                <th class="cell"></th>
+            </tr>
+            <tr>
+                <th class="cell"></th>
+                <th class="cell"></th>
+                <th class="cell"></th>
+            </tr>
+            <tr>
+                <th class="cell"></th>
+                <th class="cell"></th>
+                <th class="cell"></th>
+            </tr>
+            <!-- <div class="cell">
+
             </div>
             <div class="cell">
-                
+
             </div>
             <div class="cell">
-                
+
             </div>
             <div class="cell">
-                
+
             </div>
             <div class="cell">
-                
+
             </div>
             <div class="cell">
-                
+
             </div>
             <div class="cell">
-                
+
             </div>
             <div class="cell">
-                
+
             </div>
             <div class="cell">
-                
-            </div>
-        </div>
-        
+
+            </div> -->
+        </table>
+
         <img src="assets/img/arrow.png" id="arrow">
-        
+
         <div class="result">
-            
+
         </div>
     </main>
 
