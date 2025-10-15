feb0c70bcfd7b974c835e086c587b1e4ea26cbc5
mwepbc <just.natsuki@bk.ru>
2025-10-15 11:00:02 +0500

    add a visual display of craft! (means index.php is done now)

---
+++ assets/functions/crafts.php
@@ -0,0 +1,88 @@
+<?php
+
+include('connect.php');
+$input = file_get_contents('php://input');
+$data = json_decode($input, true);
+
+
+function allCrafts($dbh)
+{
+    $sth = $dbh->prepare('SELECT i.id, i.name, i.image
+            FROM crafts c
+            JOIN items i ON c.crafting_item = i.id');
+    $sth->execute();
+    echo json_encode($sth->fetchAll());
+}
+
+// по айдишнику крафтящегося предмета выдается изображения предметов
+function defeniteCraft($dbh, $id)
+{
+    $sth = $dbh->prepare('
+        SELECT 
+                c.*,
+                i_res.name AS result_name,
+                i_res.image AS result_image,
+                i1.image AS image1,
+                i2.image AS image2,
+                i3.image AS image3,
+                i4.image AS image4,
+                i5.image AS image5,
+                i6.image AS image6,
+                i7.image AS image7,
+                i8.image AS image8,
+                i9.image AS image9
+            FROM crafts c
+            JOIN items i_res ON c.crafting_item = i_res.id
+            LEFT JOIN items i1 ON c.pos_1 = i1.id
+            LEFT JOIN items i2 ON c.pos_2 = i2.id
+            LEFT JOIN items i3 ON c.pos_3 = i3.id
+            LEFT JOIN items i4 ON c.pos_4 = i4.id
+            LEFT JOIN items i5 ON c.pos_5 = i5.id
+            LEFT JOIN items i6 ON c.pos_6 = i6.id
+            LEFT JOIN items i7 ON c.pos_7 = i7.id
+            LEFT JOIN items i8 ON c.pos_8 = i8.id
+            LEFT JOIN items i9 ON c.pos_9 = i9.id
+            WHERE c.crafting_item = ?
+    ');
+    $sth->execute([$id]);
+    $craft = $sth->fetch();
+
+    $result = [
+        'result' => $craft['result_image'],
+        'slot1' => $craft['image1'] ?? null,
+        'slot2' => $craft['image2'] ?? null,
+        'slot3' => $craft['image3'] ?? null,
+        'slot4' => $craft['image4'] ?? null,
+        'slot5' => $craft['image5'] ?? null,
+        'slot6' => $craft['image6'] ?? null,
+        'slot7' => $craft['image7'] ?? null,
+        'slot8' => $craft['image8'] ?? null,
+        'slot9' => $craft['image9'] ?? null,
+        'quantity' => $craft['quantity'] ?? null
+    ];
+
+    echo json_encode($result);
+}
+
+// функция вставки юзера в бд
+function insertCraft($dbh, $login, $password, $role){
+    $sth = $dbh->prepare('INSERT INTO `users`
+    (`id`, `login`, `password`, `role`)
+    VALUES (NULL, ?, ?, ?)');
+    $sth->execute([$login, $password, $role]);
+}
+
+switch ($data['function']) {
+    case 'allCrafts':
+        allCrafts($dbh);
+        break;
+
+    case 'defeniteCraft':
+        defeniteCraft($dbh,(int)$data['id_item']);
+        break;
+    
+    default:
+        echo 'oopseeee';
+        break;
+}
+?>
--- index.php
+++ index.php
@@ -82,18 +82,39 @@
         <table class="craftingTable">
             <tr>
                 <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+            </tr>
+            <!-- <tr>
+                <td>
                     <div class="cell">
-
                     </div>
                 </td>
                 <td>
                     <div class="cell">
-
                     </div>
                 </td>
                 <td>
                     <div class="cell">
-
                     </div>
                 </td>
             </tr>
@@ -110,21 +131,7 @@
                     <div class="cell">
                     </div>
                 </td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="cell">
-                    </div>
-                </td>
-                <td>
-                    <div class="cell">
-                    </div>
-                </td>
-                <td>
-                    <div class="cell">
-                    </div>
-                </td>
-            </tr>
+            </tr> -->
         </table>
 
         <img src="assets/img/arrow.png" id="arrow">
@@ -143,8 +150,6 @@
         try {
             const response = await fetch(request);
             const result = await response.json();
-
-            console.log("Success:", result);
             list.innerHTML = '';
 
             // создается массив строк, которые будут у нас tr
@@ -171,17 +176,18 @@
                     }
 
                     td.addEventListener('click', (event) => {
-                        const request2 = new Request("assets/functions/items/selectDef.php", {
+                        const requestDif = new Request("assets/functions/crafts.php", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json",
                             },
                             body: JSON.stringify({
-                                id_item: event.target.getAttribute('id_item')
+                                id_item: event.target.getAttribute('id_item'),
+                                function: 'defeniteCraft'
                             })
                         });
 
-                        postDif(request2);
+                        postDif(requestDif);
                     });
 
                     tr.appendChild(td);
@@ -199,22 +205,43 @@
         }
     }
 
-    const request1 = new Request("assets/functions/items/select.php", {
-        method: "GET",
+    const request1 = new Request("assets/functions/crafts.php", {
+        method: "POST",
         headers: {
             "Content-Type": "application/json",
-        }
+        },
+        body: JSON.stringify({
+            function: "allCrafts"
+        }),
     });
 
     post(request1);
 
-    // post для отправки selectDif и отображения крафта
+    const craftingCells = document.querySelectorAll('.craftingTable td');
+    const resultCell = document.querySelector('.result');
+
+    //post для отправки selectDif и отображения крафта
     async function postDif(request) {
         try {
             const response = await fetch(request);
             const result = await response.json();
             console.log("Success:", result);
 
+            craftingCells[0].innerHTML = `<img src = "assets/img/${result.slot1}" alt="" >`
+            craftingCells[1].innerHTML = `<img src = "assets/img/${result.slot2}" alt="" >`
+            craftingCells[2].innerHTML = `<img src = "assets/img/${result.slot3}" alt="" >`
+            craftingCells[3].innerHTML = `<img src = "assets/img/${result.slot4}" alt="" >`
+            craftingCells[4].innerHTML = `<img src = "assets/img/${result.slot5}" alt="" >`
+            craftingCells[5].innerHTML = `<img src = "assets/img/${result.slot6}" alt="" >`
+            craftingCells[6].innerHTML = `<img src = "assets/img/${result.slot7}" alt="" >`
+            craftingCells[7].innerHTML = `<img src = "assets/img/${result.slot8}" alt="" >`
+            craftingCells[8].innerHTML = `<img src = "assets/img/${result.slot9}" alt="" >`
+
+            resultCell.innerHTML = `<img src="assets/img/${result.result}" alt="">`;
+
+            if (result.quantity != null) {
+                resultCell.innerHTML += `<p>${result.quantity}</p>`;
+            }
         } catch (error) {
             console.error("Error:", error);
         }
