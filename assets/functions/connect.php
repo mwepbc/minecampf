0bb613e0fb2b1c02338ec8e7ee1894ebb62573a2
mwepbc <just.natsuki@bk.ru>
2025-10-18 19:03:53 +0500

    rename project to more ethic way

--- assets/functions/connect.php
+++ assets/functions/connect.php
@@ -1,7 +1,7 @@
 <?php
 
 // Подключение к базе
-$dsn = 'mysql:dbname=minecampf;host=127.0.0.1;port=3306';
+$dsn = 'mysql:dbname=minecraft;host=127.0.0.1;port=3306';
 // для опенсервера
 // $dsn = 'mysql:dbname=minecampf;host=127.0.0.1;port=3307';
 $user = 'root';
@@ -12,8 +12,6 @@ try {
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]);
-
 } catch (PDOException $e) {
     die("Ошибка подключения: " . $e->getMessage());
 }
-?>
